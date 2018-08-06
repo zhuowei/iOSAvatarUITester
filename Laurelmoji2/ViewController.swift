b367d1b3f64d293e6012b4ee22048b7d6cfda580
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2018-08-05 22:57:31 -0400

    Initial Commit

---
+++ Laurelmoji2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Laurelmoji2
+//
+//  Created by Zhuowei Zhang on 2018-08-05.
+//  Copyright © 2018 Zhuowei Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
